581afe5560406ef988904c2f8b24382fa2a2e1d0
Brend3723 <zommi2710@gmail.com>
2024-03-21 20:03:56 -0600

    CyB<3

---
+++ proyecto/proyecto/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using proyecto.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace proyecto.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
